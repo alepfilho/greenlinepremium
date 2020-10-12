62c5f60ed8272984d7fad67e1495d45dd0446f01
alepfilho <alexandrepoltronieri@gmail.com>
2020-10-12 10:10:32 -0300

    adicionado comparadores para descobrir qual produto será recomendado

--- assets/js/informacoes.js
+++ assets/js/informacoes.js
@@ -7,28 +7,35 @@ $("#next-1").click(() => {
         nome: nome
     }
     $('.nome-pessoa').text(respostas.nome)
-    if(respostas.nome == ''){        
+    if (respostas.nome == '') {
         alert('Por favor, Diga-nos o seu nome')
-    }else{
-    $('#pills-profile-tab').click()
-        
+    } else {
+        $('#pills-profile-tab').click()
+
     }
 })
-$("#next-2").click(function(){
+$("#next-2").click(function () {
     const selected = new Array();
     $("#checkbox-objetivos input[type=checkbox]:checked").each(function () {
         selected.push(this.value);
         respostas.escolhas = selected
     });
 
-    if(selected.length == 0){
+    if (selected.length == 0) {
         alert('Por favor, escolha um pelo menos')
-    }else{
-        
-    $('#pills-contact-tab').click()
+    } else {
 
-    const newArray = respostas.escolhas
+        $('#pills-contact-tab').click()
 
+        const newArray = respostas.escolhas;
+        console.log(newArray.indexOf('emagrecer'))
+        console.log('length:' + newArray.length)
+
+        if (newArray.indexOf('emagrecer') || newArray.length == 1) {
+            alert('Tem somente emagrecer')
+        } else if(newArray.indexOf('emagrecer') || newArray.indexOf('hipertrofia') || newArray.length == 2){
+            alert('Emagrecer e hipertrofia')
+        }
     }
 
 })
